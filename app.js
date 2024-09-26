b8452bf83a41eba960f81e7e7a33959052986728
harish04ss <harishsubramanian4122001@gmail.com>
2024-09-26 15:46:35 +0530

    host added for admin and staff too

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ const app = express();
 const cors = require('cors');
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://main--peppy-dolphin-89bcee.netlify.app'
+  'https://main--peppy-dolphin-89bcee.netlify.app',
+  'https://main--peppy-dolphin-89bcee.netlify.app/admin',
+  'https://main--peppy-dolphin-89bcee.netlify.app/staff',
+
 ];
 // app.use(cors({
 //     origin: 'http://localhost:5173',
