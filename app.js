98c60d15a07a8f34c8c19ab08c038583c8dcef43
harish04ss <harishsubramanian4122001@gmail.com>
2024-10-02 01:17:20 +0530

    resolving

--- app.js
+++ app.js
@@ -1,44 +1,94 @@
+// const express = require('express');
+// const residentRouter = require('./routes/residentRoute');
+// const authRouter = require('./routes/authRoute');
+// const adminRouter = require('./routes/adminRoute')
+// const staffRouter = require('./routes/staffRoute')
+// const paymentRouter = require('./routes/paymentRoute')
+// const requestRouter = require('./routes/requestRoute');
+// const fetchreqAssingRouter = require('./routes/fetchreqAssingRoute')
+// const app = express();
+// const cors = require('cors');
+// const allowedOrigins = [
+//   'http://localhost:5173',
+//   'https://main--peppy-dolphin-89bcee.netlify.app',
+//   'https://main--peppy-dolphin-89bcee.netlify.app/admin',
+//   'https://main--peppy-dolphin-89bcee.netlify.app/staff',
+// ];
+//  app.use(cors({
+//      origin: 'http://localhost:5173',
+//      credentials: true, 
+//      methods:['GET','POST','PATCH','PUT','DELETE']
+//  }));
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
+// }));
+// app.use(express.json());
+// app.get('/', (req, res) => {
+//     res.send('Welcome to the API!');
+//   });
+// app.use('/api/v1/auth',authRouter)
+// app.use('/api/v1/admin', adminRouter);
+// //app.use('/api/customer', customerRoutes);
+// app.use('/api/v1/staff', staffRouter);
+// app.use('/api/v1/payment', paymentRouter);
+// app.use('/api/v1',requestRouter);
+// app.use('/api/v1',fetchreqAssingRouter);
+// module.exports = app;
 const express = require('express');
 const residentRouter = require('./routes/residentRoute');
 const authRouter = require('./routes/authRoute');
-const adminRouter = require('./routes/adminRoute')
-const staffRouter = require('./routes/staffRoute')
-const paymentRouter = require('./routes/paymentRoute')
+const adminRouter = require('./routes/adminRoute');
+const staffRouter = require('./routes/staffRoute');
+const paymentRouter = require('./routes/paymentRoute');
 const requestRouter = require('./routes/requestRoute');
-const fetchreqAssingRouter = require('./routes/fetchreqAssingRoute')
-const app = express();
+const fetchreqAssingRouter = require('./routes/fetchreqAssingRoute');
 const cors = require('cors');
+const app = express();
+
+// List of allowed origins
 const allowedOrigins = [
   'http://localhost:5173',
   'https://main--peppy-dolphin-89bcee.netlify.app',
   'https://main--peppy-dolphin-89bcee.netlify.app/admin',
   'https://main--peppy-dolphin-89bcee.netlify.app/staff',
 ];
- app.use(cors({
-     origin: 'http://localhost:5173',
-     credentials: true, 
-     methods:['GET','POST','PATCH','PUT','DELETE']
- }));
+
+// Configure CORS middleware
 app.use(cors({
   origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps, curl requests)
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true,
-  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE']
+  credentials: true,  // Allow cookies and other credentials
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'],  // Allowed headers
 }));
-app.use(express.json());
+
+app.use(express.json());  // Body parser
+
+// Default route
 app.get('/', (req, res) => {
-    res.send('Welcome to the API!');
-  });
-app.use('/api/v1/auth',authRouter)
+  res.send('Welcome to the API!');
+});
+
+// Route setup
+app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/admin', adminRouter);
-//app.use('/api/customer', customerRoutes);
 app.use('/api/v1/staff', staffRouter);
 app.use('/api/v1/payment', paymentRouter);
-app.use('/api/v1',requestRouter);
-app.use('/api/v1',fetchreqAssingRouter);
+app.use('/api/v1', requestRouter);
+app.use('/api/v1', fetchreqAssingRouter);
+
 module.exports = app;
